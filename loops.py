bcd1283cdb3a9ad9d11726158aad6a9b5bee5bd3
Dalton <dalton.j.mcauliffe97@gmail.com>
2026-07-08 15:37:11 -0700

    used a for loop to dertemine odd's or evens in a range

---
+++ loops.py
@@ -0,0 +1,9 @@
+for i in range(1,6):
+    print(f"{i + 1}. set your heart ablaze!")
+for i in range(10):
+
+    if i % 2 == 0:
+        print(f"{i} is even")
+
+    else:
+        print(f"{i} is odd")
